50b1df66a7abe5bdca2b414884f2567f107f5f08
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-23 16:30:15 +0530

    string practice questions added

---
+++ src/String_and_StringBuilder/Solution.java
@@ -0,0 +1,19 @@
+package String_and_StringBuilder;
+
+class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String test(){
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < indices.length; i++) {
+            ans.append(s.charAt(i));
+        }
+        ans.append(s.charAt(0));
+        return (String)ans;
+    }
+
+}
