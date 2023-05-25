60125dd226199d5143f7e1b71b44bf33035159dd
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-25 17:33:45 +0530

    binary search using recursion added

---
+++ src/Recursion/Learn/binarySeachUsingRecursion.java
@@ -0,0 +1,21 @@
+package Recursion.Learn;
+
+public class binarySeachUsingRecursion {
+    public static void main(String[] args) {
+        int[] arr = {4,8,2,3,6,9,0,2};
+        System.out.println(recursionBS(arr,0,arr.length,0));
+    }
+    static int recursionBS(int[] arr, int start, int end, int target){
+        if(start > end){
+            return -1;
+        }
+        int mid = start + (end-start)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        if(target < arr[mid]){
+            return recursionBS(arr,mid+1,end,target);
+        }
+        return recursionBS(arr,start,mid-1,target);
+    }
+}
