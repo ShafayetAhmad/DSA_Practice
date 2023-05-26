06f82e6b93df30085b7e9dd175fdbaab87d13fb6
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-26 20:25:49 +0530

    pattern solutions added

---
+++ src/Patterns/Practice/patterns.java
@@ -0,0 +1,52 @@
+package Patterns.Practice;
+
+public class patterns {
+    public static void main(String[] args) {
+        pattern5(6);
+    }
+
+    static void pattern1(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j ++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = n-i; j >0; j--){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+
+
+
+
+
+
+    }
+}
