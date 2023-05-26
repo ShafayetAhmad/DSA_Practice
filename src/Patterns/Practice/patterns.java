91f91d703ee9ffb35442e2677b60f55a1e27437d
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-26 20:31:25 +0530

    5th pattern added

--- src/Patterns/Practice/patterns.java
+++ src/Patterns/Practice/patterns.java
@@ -5,48 +5,58 @@ public class patterns {
         pattern5(6);
     }
 
-    static void pattern1(int n){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j ++){
+    static void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print("*" + " ");
             }
             System.out.println();
         }
     }
 
-    static void pattern2(int n){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
+    static void pattern2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("*" + " ");
             }
             System.out.println();
         }
     }
 
-    static void pattern3(int n){
-        for(int i = 0; i < n; i++){
-            for(int j = n-i; j >0; j--){
+    static void pattern3(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 0; j--) {
                 System.out.print("*" + " ");
             }
             System.out.println();
         }
     }
 
-    static void pattern4(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+    static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
     }
 
-    static void pattern5(int n){
+    static void pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
 
+        }
+        for (int i = n; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
 
-
-
-
-
+        }
     }
+
+
 }
