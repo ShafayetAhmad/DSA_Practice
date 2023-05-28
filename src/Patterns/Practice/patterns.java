aa5fe7419fca2e65ae0b07094f20223e0c22e36c
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-28 23:45:56 +0530

    pattern 7 added

--- src/Patterns/Practice/patterns.java
+++ src/Patterns/Practice/patterns.java
@@ -2,7 +2,7 @@ package Patterns.Practice;
 
 public class patterns {
     public static void main(String[] args) {
-        pattern6(6);
+        pattern7(6);
     }
 
     static void pattern1(int n) {
@@ -69,6 +69,20 @@ public class patterns {
                 System.out.print("*"+" ");
             }
 
+            System.out.println();
+        }
+
+    }
+
+    static void pattern7(int n) {
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j ++){
+                System.out.print(" "+" ");
+            }
+            for(int k = n-i; k > 0; k--){
+                System.out.print("*"+" ");
+            }
             System.out.println();
         }
 
