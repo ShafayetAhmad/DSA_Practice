e8c177248e5acb9ec9ad8ee9d1bab156a4c758f4
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-05-29 21:10:39 +0530

    pattern 13 added

--- src/Patterns/Practice/patterns.java
+++ src/Patterns/Practice/patterns.java
@@ -174,8 +174,29 @@ public class patterns {
         }
     }
 
+    static void pattern13(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = n-i; j > 0 ; j --){
+                System.out.print(" ");
+            }
+            for(int k = i*2+1; k > 0 ; k--){
+                if(k == i*2+1 || k == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+            if(i == n-1){
+                for(int j = 0; j <= n; j++){
+                    System.out.print("*"+" ");
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        pattern12(6);
+        pattern13(6);
     }
 
 
