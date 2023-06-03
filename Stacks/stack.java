6c7ecb97cffef7f6220a5974a86016864663ed60
ShafayetAhmad <shafayetahmad1@gmail.com>
2023-06-03 23:32:45 +0530

    queue using array added

--- Stacks/stack.java
+++ Stacks/stack.java
@@ -7,11 +7,14 @@ public class stack {
         stack.push(2);
         stack.push(3);
         stack.push(4);
+        stack.push(5);
+        stack.push(6);
         System.out.println(stack.peek());
         System.out.println(stack.pop());
         System.out.println(stack.peek());
         System.out.println(stack.size());
         System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
